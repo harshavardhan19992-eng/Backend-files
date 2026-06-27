fc6323a0482a5f9f97c7404e9ebdc99a5385a10d
harshavardhan19992-eng <harshavardhan19992@gmail.com>
2026-06-27 17:06:28 +0530

    Refactor Booking schema with type changes
    Updated Booking schema to change petAge and petWeight types to String and reordered petType.

--- models/Booking.js
+++ models/Booking.js
@@ -1,41 +1,35 @@
 const mongoose = require("mongoose");
 
-const BookingSchema = new mongoose.Schema({
-
+const bookingSchema = new mongoose.Schema({
   ownerName: String,
   phone: String,
   email: String,
 
   petName: String,
-  petType: String,
   breed: String,
-  petAge: Number,
-  petWeight: Number,
+  petAge: String,
+  petWeight: String,
+  petType: String,
 
   service: String,
-
   date: String,
   timeSlot: String,
 
   address: String,
   notes: String,
 
+  price: {
+    type: Number,
+    default: 0
+  },
+
   status: {
     type: String,
     default: "Pending"
   }
 
-  price: {
-  type: Number,
-  default: 0
-}
-
 }, {
   timestamps: true
 });
 
-module.exports =
-  mongoose.model(
-    "Booking",
-    BookingSchema
-  );
+module.exports = mongoose.model("Booking", bookingSchema);
